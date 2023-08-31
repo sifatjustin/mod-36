3aabf4a6e1c20bfd8abf64364992349218c8c345
sifatjustin <sifatjustin@gmail.com>
2023-09-01 00:17:32 +0600

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const handleCategory = async ()=>{
+const handleCategory = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
 
+  const tabContainer = document.getElementById("tab-container");
 
-const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-const data = await res.json();
-
-const tabContainer = document.getElementById('tab-container')
-
-data.data.forEach(category => {
-    const div = document.createElement('div')
+  data.data.forEach((category) => {
+    const div = document.createElement("div");
     div.innerHTML = `
-    <a class="tab">${category.category}</a>
-    `
-    tabContainer.appendChild(div)
-});
+    <a onclick = "handleLoadVideos('${category.category_id}')" class="tab">${category.category}</a>
+    `;
+    tabContainer.appendChild(div);
+  });
 
-// console.log(data.data[2].category);
-}
+  // console.log(data.data[2].category);
+};
 
-handleCategory()
+const handleLoadVideos = async (categoryID) => {
+  // console.log(categoryID)
+  const res = await fetch(`
+  https://openapi.programming-hero.com/api/videos/category/${categoryID}
+  `)
+  const data = await res.json()
+  console.log(data.data);
+};
+
+handleCategory();
